8c6206b608a87a631c95971af5840c002dcf83ee
Eugenio <eugeniolopesarantesjunior@gmail.com>
2023-06-19 10:18:39 -0300

    adding findAll products route

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -4,6 +4,7 @@ import { prisma } from '../database/prismaClient';
 
 const productsRouter = Router();
 
+//CREATE
 productsRouter.post('/', async (request, reply) => {
   const registerBodySchema = z.object({
     name: z.string(),
@@ -19,5 +20,25 @@ productsRouter.post('/', async (request, reply) => {
 
   return reply.status(201).send()
 })
+
+//FIND ALL
+productsRouter.get('/', async (_, reply) => {
+  const products = await prisma.products.findMany();
+
+  return reply.status(201).send(JSON.stringify(products));
+})
+
+//FIND BY ID
+productsRouter.get('/:id', async (request, reply) => {
+  const { id } = request.params;
+
+  const product = await prisma.products.findUnique({
+    where: {
+      id,
+    }
+  });
+
+  return reply.status(200).send(product);
+});
 
 export default productsRouter;
