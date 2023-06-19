5f4a5866bde30aaf959c84b0c7518e12e099f53a
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2023-06-19 16:21:29 -0300

    adjusting productSale routes

--- src/routes/productSale.routes.ts
+++ src/routes/productSale.routes.ts
@@ -28,59 +28,14 @@ productSaleRouter.post('/', async (request, response) => {
 // READ
 // -- List All
 productSaleRouter.get('/', async (_request, response) => {
-  const productSale = await prisma.productSale.findMany();
-
-  return response.status(200).send(productSale);
-});
-
-// -- Find By Id
-productSaleRouter.get('/:productId/:saleId', async (request, response) => {
-  const { productId, saleId } = request.params;
-
-  // const productSale = await prisma.productSale.findUnique({
-  //   where: {
-  //     productId,
-  //     saleId
-  //   }
-  // });
-
-  // return response.status(200).send(productSale);
-});
-
-// UPDATE
-productSaleRouter.put('/', async (request, response) => {
-  const registerBodySchema = z.object({
-    productId: z.string(),
-    saleId: z.string(),
-    amount: z.number(),
-  });
-
-  const { productId, saleId, amount } = registerBodySchema.parse(request.body);
-
-  // const productSale = await prisma.productSale.update({
-  //   where: {
-  //     productId_saleId: `${productId}${saleId}`,
-  //   },
-  //   data: {
-  //     name,
-  //     salary,
-  //   }
-  // });
-
-  // return response.status(200).send(productSale);
-});
-
-// DELETE
-productSaleRouter.delete('/:id', async (request, response) => {
-  const { id } = request.params;
-
-  const position = await prisma.positions.delete({
-    where: {
-      id,
+  const productSale = await prisma.productSale.findMany({
+    include: {
+      product: true,
+      sale: true,
     }
   });
 
-  return response.status(200).send(position);
+  return response.status(200).send(productSale);
 });
 
 export default productSaleRouter;
