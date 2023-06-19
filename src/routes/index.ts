88f07b0109605ebb588e7fb7697eff3719931754
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2023-06-19 16:35:23 -0300

    unitSupplier routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,13 @@ import productsRouter from './products.routes'
 import positionsRouter from './positions.routes'
 import salesRouter from "./sales.routes";
 import customersRouter from "./customers.routes";
-import productSaleRouter from "./productSale.routes";
 import unitsRouter from './units.routes';
 import suppliersRouter from './suppliers.routes';
 import equipmentRouter from './equipment.routes';
 import employeesRouter from './employees.routes';
+
+import productSaleRouter from "./productSale.routes";
+import unitSupplierRouter from "./unitSupplier.routes";
 
 const routes = Router();
 
@@ -19,7 +21,9 @@ routes.use('/products', productsRouter);
 routes.use('/positions', positionsRouter);
 routes.use('/sales', salesRouter);
 routes.use('/customers', customersRouter);
-routes.use('/product_sale', productSaleRouter);
 routes.use('/employees', employeesRouter);
+
+routes.use('/product_sale', productSaleRouter);
+routes.use('/unit_supplier', unitSupplierRouter);
 
 export default routes;
---
+++ src/routes/unitSupplier.routes.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import { z } from 'zod';
+import { prisma } from '../database/prismaClient';
+
+const unitSupplierRouter = Router();
+
+// CREATE
+unitSupplierRouter.post('/', async (request, response) => {
+  const registerBodySchema = z.object({
+    unitId: z.string(),
+    supplierId: z.string(),
+  });
+
+  const { unitId, supplierId } = registerBodySchema.parse(request.body);
+
+  const unitSupplier = await prisma.unitSupplier.create({
+    data: {
+      unitId,
+      supplierId,
+    }
+  });
+
+  return response.status(201).send(unitSupplier);
+});
+
+// READ
+// -- List All
+unitSupplierRouter.get('/', async (_request, response) => {
+  const unitSupplier = await prisma.unitSupplier.findMany({
+    include: {
+      unit: true,
+      supplier: true,
+    }
+  });
+
+  return response.status(200).send(unitSupplier);
+});
+
+export default unitSupplierRouter;
