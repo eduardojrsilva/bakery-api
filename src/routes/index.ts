c3e1e18e784041cc0f8c278e5f010611a37e76f7
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2023-06-19 11:40:00 -0300

    positions routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import productsRouter from './products.routes'
+import positionsRouter from './positions.routes'
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+routes.use('/positions', positionsRouter);
 
 export default routes;
---
+++ src/routes/positions.routes.ts
@@ -0,0 +1,83 @@
+import { Router } from 'express';
+import { z } from 'zod';
+import { prisma } from '../database/prismaClient';
+
+const positionsRouter = Router();
+
+// CREATE
+positionsRouter.post('/', async (request, response) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    salary: z.string(),
+  });
+
+  const { name, salary } = registerBodySchema.parse(request.body);
+
+  const position = await prisma.positions.create({
+    data: {
+      name,
+      salary,
+    }
+  });
+
+  return response.status(201).send(position);
+});
+
+// READ
+// -- List All
+positionsRouter.get('/', async (_request, response) => {
+  const positions = await prisma.positions.findMany();
+
+  return response.status(200).send(positions);
+});
+
+// -- Find By Id
+positionsRouter.get('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const position = await prisma.positions.findUnique({
+    where: {
+      id,
+    }
+  });
+
+  return response.status(200).send(position);
+});
+
+// UPDATE
+positionsRouter.put('/', async (request, response) => {
+  const registerBodySchema = z.object({
+    id: z.string(),
+    name: z.string().optional(),
+    salary: z.string().optional(),
+  });
+
+  const { id, name, salary } = registerBodySchema.parse(request.body);
+
+  const position = await prisma.positions.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      salary,
+    }
+  });
+
+  return response.status(200).send(position);
+});
+
+// DELETE
+positionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const position = await prisma.positions.delete({
+    where: {
+      id,
+    }
+  });
+
+  return response.status(200).send(position);
+});
+
+export default positionsRouter;
