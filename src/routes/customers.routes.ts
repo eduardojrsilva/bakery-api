66f0eb5b4534999ebb907ae8d74e26f277e805a2
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2023-06-19 12:27:33 -0300

    customers routes

---
+++ src/routes/customers.routes.ts
@@ -0,0 +1,79 @@
+import { Router } from 'express';
+import { z } from 'zod';
+import { prisma } from '../database/prismaClient';
+
+const customersRouter = Router();
+
+// CREATE
+customersRouter.post('/', async (request, response) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+  });
+
+  const { name } = registerBodySchema.parse(request.body);
+
+  const customer = await prisma.customers.create({
+    data: {
+      name,
+    }
+  });
+
+  return response.status(201).send(customer);
+});
+
+// READ
+// -- List All
+customersRouter.get('/', async (_request, response) => {
+  const customers = await prisma.customers.findMany();
+
+  return response.status(200).send(customers);
+});
+
+// -- Find By Id
+customersRouter.get('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const customer = await prisma.customers.findUnique({
+    where: {
+      id,
+    }
+  });
+
+  return response.status(200).send(customer);
+});
+
+// UPDATE
+customersRouter.put('/', async (request, response) => {
+  const registerBodySchema = z.object({
+    id: z.string(),
+    name: z.string().optional(),
+  });
+
+  const { id, name } = registerBodySchema.parse(request.body);
+
+  const customer = await prisma.customers.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+    }
+  });
+
+  return response.status(200).send(customer);
+});
+
+// DELETE
+customersRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const customer = await prisma.customers.delete({
+    where: {
+      id,
+    }
+  });
+
+  return response.status(200).send(customer);
+});
+
+export default customersRouter;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import productsRouter from './products.routes'
 import positionsRouter from './positions.routes'
 import salesRouter from "./sales.routes";
+import customersRouter from "./customers.routes";
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/positions', positionsRouter);
 routes.use('/sales', salesRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
